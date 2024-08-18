20d5cb25266477eaa056c82775f88558466403c3
kanondrago <ronaldchavezr@gmail.com>
2024-08-18 20:46:58 +0200

    espacios 2

--- src/App.vue
+++ src/App.vue
@@ -88,6 +88,7 @@
 
       <img class="header-guitarra" src="/img/header_guitarra.png" alt="imagen header">
   </header>
+
   <main class="container-xl mt-5">
       <h2 class="text-center">Nuestra Colección</h2>
 
@@ -268,6 +269,7 @@
           </div><!-- FIN GUITARRA -->
       </div>
   </main>
+  
   <footer class="bg-dark mt-5 py-5">
       <div class="container-xl">
           <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
