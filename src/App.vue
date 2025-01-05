61ab405216a2a2c89aec3fd1c6b21003f0186048
kanondrago <ronaldchavezr@gmail.com>
2025-01-05 15:14:41 +0100

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+  import {ref, reactive, onMounted} from 'vue';
+  import {db} from './data/guitarras'
+
+  import Header from './components/Header.vue';
+  import Guitarra from './components/Guitarra.vue';
+
+  const guitarras = ref([]);
+  
+  onMounted( () => {
+    guitarras.value = db;
+  })
+  
+
+</script>
+
+<template>
+
+    <Header></Header>
+
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">
+          <Guitarra 
+            :key="guitarra.id"
+            v-for="guitarra in guitarras"
+            v-bind:guitarrax="guitarra"> 
+          </Guitarra>
+        </div>
+
+    </main>
+
+    <footer class="bg-dark mt-5 py-5">
+        <div class="container-xl">
+            <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+
+</template>
---
+++ src/components/Guitarra.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+    // Macro --> no requieren importarse
+    const props = defineProps({
+        guitarrax: {
+            type: Object,
+            required: true,
+        }
+    })
+    
+
+</script>
+
+<template>
+
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+            <img class="img-fluid" v-bind:src="`../../public/img/${guitarrax.imagen}.jpg`" v-bind:alt="`imagen guitarra: ${guitarrax.nombre}`">
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{guitarrax.nombre}}</h3>
+            <p>{{ guitarrax.descripcion }}</p>
+            <p class="fw-black text-primary fs-3">$ {{ guitarrax.precio }}</p>
+            <button 
+                type="button"
+                class="btn btn-dark w-100 "
+            >Agregar al Carrito</button>
+        </div>
+    </div>
+
+</template>
